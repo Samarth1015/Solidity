63c8517f4cf1bd0a673b7f44855614337e707316
samarth <202351125@iiitvadodara.ac.in>
2025-01-10 17:49:49 +0530

    init

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,32 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-contract Contract { }
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "forge-std/console.sol";
+contract Counter is ERC20, Ownable
+ {
+    
+   constructor() ERC20("SamarthCoin","SAM") Ownable(msg.sender){
+    
+    
+   }
+
+function mint(address _add,uint val) public onlyOwner {
+    console.logString("hello from mint");
+    _mint(_add, val);
+
+}
+function trans(address to,uint val) public{
+    transfer(to, val);
+}
+
+
+   
+   
+
+   
+  
+
+}
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -3,21 +3,67 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
-import "src/Contract.sol";
+import "../src/Contract.sol";
+
+
 
 contract TestContract is Test {
-    Contract c;
+   event Transfer(address indexed from, address indexed to, uint256 value);
+
+    Counter c;
 
     function setUp() public {
-        c = new Contract();
+       c = new Counter();
     }
+function testminting() public{
+   c.mint(address(this), 100);
+   
+   assertEq(c.balanceOf(address(this)),uint256(100),"ok" );
 
-    function testBar() public {
-        assertEq(uint256(1), uint256(1), "ok");
-    }
 
-    function testFoo(uint256 x) public {
-        vm.assume(x < type(uint128).max);
-        assertEq(x + x, x * 2);
-    }
+   assertEq(c.balanceOf(0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD), 0,"ok");
 }
+
+function testTransfer() public{
+
+   c.mint(address(this), 100);
+
+   uint256 bal=c.balanceOf(0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD);
+   c.trans(0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD, 10);
+      assertTrue(bal+10 == (c.balanceOf(0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD)));
+
+         vm.prank(0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD);
+      c.trans(address(this), 10);
+      assertEq(c.balanceOf(address(this)),100,"ok" );
+      assertEq(c.balanceOf    (0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD), 0,"ok");
+
+
+
+}
+function testApproval () public {
+   c.mint(address(this), 100);
+   c.approve(0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD, 10);
+   vm.prank(0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD);
+   c.transferFrom(address(this), 0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD, 5);
+   assertEq(c.balanceOf(address(this)),uint (95),"ok");
+   assertEq(c.balanceOf(0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD) , uint (5),"ok");
+
+   
+
+
+}
+function testEmittrasfer()public{
+   c.mint(address(this), 100);
+
+   vm.expectEmit(true, true, false, true);
+   emit Transfer(address(this), 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, 10);
+   c.trans(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, 10);
+
+
+   
+
+}
+
+   
+
+    }
